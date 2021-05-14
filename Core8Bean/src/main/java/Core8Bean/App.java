e6181594134ed684ef87f612fb2973310d9c2b95
Prathmkaushik <prathamkaushik198@gmail.com>
2021-05-14 23:39:12 +0530

    Spring core assignment

---
+++ Core8Bean/src/main/java/Core8Bean/App.java
@@ -0,0 +1,21 @@
+package Core8Bean;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	 public static void main( String[] args )
+	    {
+	    	
+	    	ConfigurableApplicationContext context = 
+	                new ClassPathXmlApplicationContext(new String[] {"applicationContext.xml"});
+	        
+	        	Employee cust = (Employee)context.getBean("emp");
+	        	
+	        	System.out.println(cust);
+	        	
+	        	context.close();
+	       
+	    }
+
+}
