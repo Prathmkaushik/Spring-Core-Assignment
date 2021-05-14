dd70b127270bff97f338e8eeae05d978bc6a37dd
Prathmkaushik <prathamkaushik198@gmail.com>
2021-05-14 23:21:16 +0530

    Spring core Assignment

---
+++ SpringExpressionLang/src/main/java/SpringExpressionLang/App.java
@@ -0,0 +1,21 @@
+package SpringExpressionLang;
+
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class App {
+	 public static void main( String[] args )
+	    {
+	        
+	    	ExpressionParser expressionParser = new SpelExpressionParser();
+
+	        Expression expression = expressionParser.parseExpression("'Hello'");
+	        String strVal = expression.getValue(String.class);
+	        System.out.println("Spring Expression Language SPEL" + strVal);
+	    	  
+
+	    	
+	    }
+
+}
